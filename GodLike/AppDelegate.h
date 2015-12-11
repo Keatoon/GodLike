5119f50238e9a0cdbbe2380d08d29a0910890680
Keatoon <1793558729@qq.com>
2015-12-11 10:40:49 +0800

    new

---
+++ GodLike/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  GodLike
+//
+//  Created by fq on 15/12/11.
+//  Copyright (c) 2015年 fangqian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ GodLike/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GodLike
+//
+//  Created by fq on 15/12/11.
+//  Copyright (c) 2015年 fangqian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
